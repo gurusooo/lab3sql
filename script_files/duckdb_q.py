5e9a6cdbe281c05947630691305f8daeb97a24d8
gurusooo <130143477+gurusooo@users.noreply.github.com>
2023-12-24 23:13:31 +0300

    Update duckdb_q.py

--- script_files/duckdb_q.py
+++ script_files/duckdb_q.py
@@ -1,39 +1,42 @@
-def execute_request(db_connection, print_flag, sql_request):
-    cursor_con = db_connection.cursor()
-    cursor_con.execute(sql_request)
-    if print_flag:
-        for i in cursor_con.fetchall():
-            print(i)
-    db_connection.commit()
-
-def duckdb_q1(con, db_table_name, print_flag=False):
+import duckdb
+def duckdb_q1(db_table_name, print_flag=False):
     sql_request = f"""SELECT \"VendorID\", count(*) 
                     FROM {db_table_name} 
                     GROUP BY \"VendorID\";"""
-    execute_request(con, print_flag, sql_request)
+    data = duckdb.sql(sql_request).fetchall()
+    if print_flag:
+        data.show()
 
-def duckdb_q2(con, db_table_name, print_flag=False):
+def duckdb_q2(db_table_name, print_flag=False):
     sql_request = f"""SELECT passenger_count, avg(total_amount) 
                     FROM {db_table_name} 
                     GROUP BY passenger_count;"""
-    execute_request(con, print_flag, sql_request)
+    data = duckdb.sql(sql_request).fetchall()
+    if print_flag:
+        data.show()
 
-def duckdb_q3(con, db_table_name, print_flag=False):
+def duckdb_q3(db_table_name, print_flag=False):
     sql_request = f"""SELECT 
                     passenger_count,
                     EXTRACT(YEAR FROM tpep_pickup_datetime),
                     count(*)
                     FROM {db_table_name}
                     GROUP BY passenger_count, EXTRACT(YEAR FROM tpep_pickup_datetime);"""
-    execute_request(con, print_flag, sql_request)
+    data = duckdb.sql(sql_request).fetchall()
+    if print_flag:
+        data.show()
 
-def duckdb_q4(con, db_table_name, print_flag=False):
+def duckdb_q4(db_table_name, print_flag=False):
     sql_request = f"""SELECT
                     passenger_count,
-                    EXTRACT(YEAR FROM pickup_datetime),
+                    EXTRACT(YEAR FROM tpep_pickup_datetime),
                     round(trip_distance),
                     count(*)
                     FROM {db_table_name}
-                    GROUP BY passenger_count, EXTRACT(YEAR FROM pickup_datetime), round(trip_distance)
-                    ORDER BY EXTRACT(YEAR FROM pickup_datetime), count(*) desc;"""
-    execute_request(con, print_flag, sql_request)
+                    GROUP BY passenger_count, EXTRACT(YEAR FROM tpep_pickup_datetime), round(trip_distance)
+                    ORDER BY EXTRACT(YEAR FROM tpep_pickup_datetime), count(*) desc;"""
+    data = duckdb.sql(sql_request).fetchall()
+    if print_flag:
+        data.show()
+
+
